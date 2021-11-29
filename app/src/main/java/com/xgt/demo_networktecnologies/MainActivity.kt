dce61f9c32309e0dd088de5d0bd93bf7905884ed
Xavi Girón <xavier.giron@live.u-tad.com>
2021-11-29 12:11:21 +0100

    Root commit

---
+++ app/src/main/java/com/xgt/demo_networktecnologies/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.xgt.demo_networktecnologies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xgt.demo_networktecnologies.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
